12362f5fe43da2784874a27c1ba80a5f277b62e3
liull <liulinlin@sunmap.com>
2014-08-29 09:29:49 +0800

    添加setup.py文件。

---
+++ setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+requires = [
+    'django',
+]
+
+# Use part of the sphinx docs index for the long description
+
+setup(
+    name="django-form-extensions",
+    version='0.0.1',
+    packages=find_packages(),
+    install_requires=requires,
+    description = 'django form extensions',
+    long_description=README,
+    author="preture",
+    author_email="preture@163.com",
+    license="BSD",
+    url="https://github.com/preture/django-form-extensions",
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
