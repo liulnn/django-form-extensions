021b7fdd4bef276ded2a8d33492e84283e7ae3a5
liull <liulinlin@sunmap.com>
2014-09-02 10:04:05 +0800

    升级版本号。

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
 
 setup(
     name="django-form-extensions",
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=requires,
     description = 'django form extensions',
